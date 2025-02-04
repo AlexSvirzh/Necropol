d01b7e647020acc6274e18b61c378921acb3be39
Dmitrii Bernikov <bernikov.dmitrii05@gmail.com>
2025-02-04 03:18:16 +0300

    Пока что итоговый вариант

--- Script1.js
+++
@@ -1,8 +0,0 @@
- JavaScript source code
-<script type="text/javascript" charset="utf-8"
-    async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aeb21e9e9cb8080bdd017f8e80261048066966a50251d7a51304d760bf3c46126&amp;
-    width=500&amp;
-    height=400&amp;
-    lang=ru_RU&amp;
-    scroll=true">
-</script>
