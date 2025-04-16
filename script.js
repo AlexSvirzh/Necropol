56cbee04e0b5e3a3cfc78179ef648bbe870c0c94
Alex Svirzh <svirzhwork@gmail.com>
2025-04-16 23:28:49 +0300

    Начал заполнять Смоленское православное(2 личности). Переделал полностью js файл.

--- script.js
+++ script.js
@@ -1,38 +1,62 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     const menuBtn = document.querySelector('.menu-btn');
+    const menuToggle = document.getElementById('menu-toggle');
 
-    window.addEventListener('scroll', function() {
-        if (window.pageYOffset > 50) { 
-            menuBtn.classList.add('menu-btn--scrolled');
-        } else {
-            menuBtn.classList.remove('menu-btn--scrolled');
-        }
-    });
-
-    document.querySelectorAll('.menu-item').forEach(item => {
-        item.addEventListener('click', () => {
-            document.getElementById('menu-toggle').checked = false; 
-        });
-    });
-    const scrollTopButton = document.getElementById("scrollTopButton");
-
-
-    function scrollToTop() {
-        document.documentElement.scrollTo({
-            top: 0,
-            behavior: 'smooth' 
+  
+    if (menuBtn && menuToggle) { 
+        menuBtn.addEventListener('click', function(event) {
+            event.preventDefault(); 
+            menuToggle.checked = !menuToggle.checked; 
         });
     }
 
+
+
     window.addEventListener('scroll', function() {
-        if (document.documentElement.scrollTop > 20) {
-            scrollTopButton.style.opacity = "1";
-            scrollTopButton.style.visibility = "visible";
-        } else {
-            scrollTopButton.style.opacity = "0";
-            scrollTopButton.style.visibility = "hidden";
+        if (menuBtn) { 
+            if (window.pageYOffset > 50) {
+                menuBtn.classList.add('menu-btn--scrolled');
+            } else {
+                menuBtn.classList.remove('menu-btn--scrolled');
+            }
+        }
+
+
+        const scrollTopButton = document.getElementById("scrollTopButton");
+        if (scrollTopButton) { 
+            if (document.documentElement.scrollTop > 20 || document.body.scrollTop > 20) {
+                scrollTopButton.style.opacity = "1";
+                scrollTopButton.style.visibility = "visible";
+            } else {
+                scrollTopButton.style.opacity = "0";
+                scrollTopButton.style.visibility = "hidden";
+            }
         }
     });
 
-    scrollTopButton.addEventListener('click', scrollToTop);
+
+    document.querySelectorAll('.menu-item').forEach(item => {
+        item.addEventListener('click', () => {
+            if (menuToggle) { 
+                 menuToggle.checked = false; 
+            }
+        });
+    });
+
+
+
+    const scrollTopButton = document.getElementById("scrollTopButton");
+
+    function scrollToTop() {
+        window.scrollTo({ 
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    if (scrollTopButton) { 
+       scrollTopButton.addEventListener('click', scrollToTop);
+    }
+
 });
