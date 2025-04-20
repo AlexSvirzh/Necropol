79cebc23154d1b9857a3ed599d303c7d7f5f831b
Alex Svirzh <svirzhwork@gmail.com>
2025-04-20 18:06:33 +0300

    Внес изменения от Сережи.

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     const menuBtn = document.querySelector('.menu-btn');
     const menuToggle = document.getElementById('menu-toggle');
+    const scrollTopButton = document.getElementById("scrollTopButton");
 
-  
-    if (menuBtn && menuToggle) { 
-        menuBtn.addEventListener('click', function(event) {
-            event.preventDefault(); 
-            menuToggle.checked = !menuToggle.checked; 
+    if (menuBtn && menuToggle) {
+        menuBtn.addEventListener('click', function (event) {
+            event.preventDefault();
+            menuToggle.checked = !menuToggle.checked;
         });
     }
 
+    document.querySelectorAll('.menu-item').forEach(item => {
+        item.addEventListener('click', () => {
+            if (menuToggle) {
+                menuToggle.checked = false;
+            }
+        });
+    });
 
+    window.addEventListener('scroll', function () {
+        const yOffset = window.pageYOffset;
 
-    window.addEventListener('scroll', function() {
-        if (menuBtn) { 
-            if (window.pageYOffset > 50) {
+        if (menuBtn) {
+            if (yOffset > 50) {
                 menuBtn.classList.add('menu-btn--scrolled');
             } else {
                 menuBtn.classList.remove('menu-btn--scrolled');
             }
         }
 
-
-        const scrollTopButton = document.getElementById("scrollTopButton");
-        if (scrollTopButton) { 
-            if (document.documentElement.scrollTop > 20 || document.body.scrollTop > 20) {
+        if (scrollTopButton) {
+            if (yOffset > 20) {
                 scrollTopButton.style.opacity = "1";
                 scrollTopButton.style.visibility = "visible";
             } else {
@@ -35,28 +41,12 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-
-    document.querySelectorAll('.menu-item').forEach(item => {
-        item.addEventListener('click', () => {
-            if (menuToggle) { 
-                 menuToggle.checked = false; 
-            }
-        });
-    });
-
-
-
-    const scrollTopButton = document.getElementById("scrollTopButton");
-
-    function scrollToTop() {
-        window.scrollTo({ 
-            top: 0,
-            behavior: 'smooth'
+    if (scrollTopButton) {
+        scrollTopButton.addEventListener('click', function () {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
         });
     }
-
-    if (scrollTopButton) { 
-       scrollTopButton.addEventListener('click', scrollToTop);
-    }
-
 });
